383c0287255ef63a23f1209597934dfc607999a5
Claudiu Mocanu <claudiu-ionut.mocanu@student.tuiasi.ro>
2026-03-23 15:43:41 +0200

    ziua 1-incepere proiect

---
+++ fitnessTrack-app/src/test/java/com/fitness/fitnessTrack_app/FitnessTrackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.fitnessTrack_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
